96474e62adcff459bc20cd5e62341ca09c2573f3
NCULineBot <102576967+NCULineBot@users.noreply.github.com>
2022-04-21 13:14:39 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+
+from linebot.models import *
+
+
+app = Flask(__name__)
+
+
+line_bot_api = LineBotApi('放lineAPI拿到的access token')
+handler = WebhookHandler('放lineAPI拿到的Channel secret')
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World!"
+
+@app.route("/", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    print("Request body: " + body, "Signature: " + signature)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+       abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    print(msg)
+    msg = msg.encode('utf-8')
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=msg))
+
+if __name__ == "__main__":
+    app.run(debug=True,port=80)
